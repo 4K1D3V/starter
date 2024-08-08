391c95e8597fa96475a55344519c73af11517ee5
GitHub Action <action@github.com>
2024-08-08 07:46:45 +0000

    Template cleanup

--- src/main/kotlin/com/github/4k1d3v/starter/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/4k1d3v/starter/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.4k1d3v.starter.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
